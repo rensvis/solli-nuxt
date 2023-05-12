339f0c997ae30a67c8ca1b868b0d1c547fb07b9b
Rens Vis <rensvis@MBP-van-Rens.home>
2023-05-12 09:53:05 +0200

    change launch date to may 22nd

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
     <div class="h-full sm:px-16 c-container">
       <div class="flex flex-col items-center justify-center w-[700px] max-w-full h-full m-auto">
         <img src="/images/solli-logo-color.png" alt="logo" class="mb-10 sm:mb-16" />
-        <h2 class="mb-6 text-2xl font-bold text-center max-[400px]:text-xl sm:text-3xl md:text-3xl sm:mb-12">Vanaf 1 juni
+        <h2 class="mb-6 text-2xl font-bold text-center max-[400px]:text-xl sm:text-3xl md:text-3xl sm:mb-12">Vanaf 22 mei
           vind je hier alle
           bijbaantjes bij jou in de
           buurt!
